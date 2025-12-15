a80584cc410e96c8b51355e5260be76ed6028ec8
Javier Castillo <javiercame98@gmail.com>
2025-12-15 13:01:24 +0100

    feat: creating reverse String method with StringBuilder and lambdas

---
+++ src/main/java/org/example/Reverse/ReverseString.java
@@ -0,0 +1,19 @@
+package org.example.Reverse;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        ReverseFunctionalInterface reverser = (string -> {
+            StringBuilder mutableString = new StringBuilder(string);
+            mutableString.reverse();
+            return mutableString.toString();
+        });
+
+
+        String originalString = "Hola mundo";
+        String reverseString = reverser.reverse(originalString);
+
+        System.out.println(reverseString);
+
+    }
+}
